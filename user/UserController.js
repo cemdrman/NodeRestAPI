ecf618a70a3db8c0727e6a061571319d25e1e509
cem dırman <cemdrman@gmail.com>
2018-06-26 15:45:34 +0300

    added new http method

--- user/UserController.js
+++ user/UserController.js
@@ -17,14 +17,37 @@ router.post('/', function (req, res) {
             res.status(200).send(user);
         });
 });
-// RETURNS ALL THE USERS IN THE DATABASE
+// RETURNS ALL THE USERS
 router.get('/', function (req, res) {
     User.find({}, function (err, users) {
         if (err) return res.status(500).send("There was a problem finding the users.");
         res.status(200).send(users);
     });
 });
+// RETURNS JUST ONE USER IF IT FIND
+router.get('/:id', function(req,res){
+    User.findById(req.params.id, function(err,res){
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
+        res.status(200).send(user);
+    });
 
+});
+// DELETES A USER
+router.delete('/:id', function (req, res) {
+    User.findByIdAndRemove(req.params.id, function (err, user) {
+        if (err) return res.status(500).send("There was a problem deleting the user.");
+        res.status(200).send("User " + user.name + " was deleted.");
+    });
+});
+// UPDATES A SINGLE USER
+router.put('/:id', function (req, res) {
+    
+    User.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, user) {
+        if (err) return res.status(500).send("There was a problem updating the user.");
+        res.status(200).send(user);
+    });
+});
 module.exports = router;
 
 /* 
