977563f581b37e6735fe3d454ba32b56dfade54b
cem dırman <cemdrman@gmail.com>
2018-06-26 16:06:09 +0300

    changed project folder structure

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var express = require("express");
 var app = express();
 var db = require("./util/db");
 
-var UserController = require('./user/UserController');
+var UserController = require('./controller/UserController');
 app.use('/users', UserController);
 
-var ProductController = require("./product/ProductController");
+var ProductController = require("./controller/ProductController");
 app.use("/product",ProductController);
 
 module.exports = app;
--- controller/ProductController.js
+++ controller/ProductController.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
-var Product = require("./Product");
+var Product = require("../model/Product");
 
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
