a030ac3841055a10f479b2c00cc46e576e0c9d08
cem dırman <cemdrman@gmail.com>
2018-06-28 00:20:37 +0300

    new routes

--- controller/UserController.js
+++ controller/UserController.js
@@ -4,17 +4,24 @@ var bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 var User = require('../model/User');
+var MailSender = require('../MailHelper')
 
 // CREATES A NEW USER
 router.post('/', function (req, res) {
     User.create({
             name : req.body.name,
+            surname: req.body.surname,
             email : req.body.email,
             password : req.body.password
         }, 
         function (err, user) {
-            if (err) return res.status(500).send("There was a problem adding the information to the database.");
-            res.status(200).send(user);
+            if (err){
+                return res.status(500).send("There was a problem adding the information to the database.");
+            }else{
+                res.status(200).send(user);
+                MailSender.sendMail('cemdrman@gmail.com','test','testttttt')
+            }
+            
         });
 });
 // RETURNS ALL THE USERS
